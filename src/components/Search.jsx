7e3a8306d12fea84b356121fdfe03fca98af57e5
Celine <obipraise2002@gmail.com>
2026-07-24 13:43:54 +0100

    Initial commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import searchIcon from '/search.png'
+const Search = ({searchTerm, setSearchTerm}) => {
+  return (
+    <div className='search'> 
+    <div>
+      <img src={searchIcon} alt='search' />
+
+      <input  
+      type="text"
+      placeholder='Search through thousands of movies'
+      value={searchTerm}
+      onChange={(event)=> setSearchTerm(event.target.value)}
+      />
+      </div>
+    </div>
+  )
+}
+
+export default Search
