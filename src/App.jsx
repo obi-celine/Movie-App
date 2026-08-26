8983c6c4c41a507810c5e7908b1db76021834181
Celine <obipraise2002@gmail.com>
2026-08-26 15:50:32 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ return (
       <div className='pattern'/>
       <div className='wrapper'>
         <header>
-         <img src={`${import.meta.env.BASE_URL}hero.png`} alt='Hero Banner' />
+         <img src={`hero.png`} alt='Hero Banner' />
           
           <h1>Find<span className='text-gradient'> Movies</span> You'll Enjoy Without the Hassle</h1>
        
