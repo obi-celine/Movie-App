cbe5b743391b830159bff07478eef796d0e4e11d
Celine <obipraise2002@gmail.com>
2026-08-28 12:57:58 -0700

    updated image file path

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ return (
       <div className='pattern'/>
       <div className='wrapper'>
         <header>
-         <img src={`/Movie-App/hero.png`} alt='Hero Banner' />
+         <img src={`/hero.png`} alt='Hero Banner' />
           
           <h1>Find<span className='text-gradient'> Movies</span> You'll Enjoy Without the Hassle</h1>
        
